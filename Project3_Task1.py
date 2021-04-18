91cccd1b567262649be8382df97c973b649ab194
Dinesh M <77041647+Dinesh3101@users.noreply.github.com>
2021-04-18 20:51:40 +0530

    Add files via upload

---
+++ Project3_Task1.py
@@ -0,0 +1,34 @@
+def most_frequent(n):
+    def count(y):
+        c=0
+        for z in ll:
+            if y==z:
+                c+=1
+            else:
+                continue
+        return c
+    def getkey(k):
+        for key,value in d.items():
+            if k==value:
+                if key not in li2:
+                    return key
+    ll=list(n)
+    d={}
+    for i in ll:
+        x=count(i)
+        u=ll.index(i)
+        d[ll[u]]=x
+    print(d)
+    li1=list(d.values())
+    li1.sort(reverse=True)
+    li2=[]
+    for j in li1:
+        v=getkey(j)
+        li2.append(v)
+    d1={}
+    for l in range(len(li2)):
+        d1[li2[l]]=li1[l]
+    for key,value in d1.items():
+        print(key,":",value)
+word=input("Please enter a string ")
+most_frequent(word)
